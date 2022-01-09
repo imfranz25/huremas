42ce70d2a5be0eec263dac40719c1a61ef26f9c5
Francis Ong <francis.ong25@gmail.com>
2022-01-09 18:06:16 +0800

    Add js fucnction for adding document details from server

--- Portal/admin/function/archive_row.php
+++ Portal/admin/function/archive_row.php
@@ -7,17 +7,24 @@
 		$archive_type = $_POST['archive_type'];
 		$table = '';
 		$reference = '';
+		$output = array();
 
 		if ($archive_type=='folder') {
 			$table = 'document_folder LEFT JOIN employees ON employees.employee_id=document_folder.folder_created_by';
 			$reference = "folder_archive=1";
+		}else if ($archive_type=='docu') {
+			$table = 'documents';
+			$reference = "document_archive=1";
 		}
 
-		$sql = "SELECT * FROM $table WHERE $reference ";
-		$query = $conn->query($sql);
-		$output = array();
-		while ($row = $query->fetch_assoc()) {
-			array_push($output, $row);
+		if ($table!='') {
+
+			$sql = "SELECT * FROM $table WHERE $reference ";
+			$query = $conn->query($sql);
+
+			while ($row = $query->fetch_assoc()) {
+				array_push($output, $row);
+			}
 		}
 
 		echo json_encode($output);
