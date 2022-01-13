e8ce560ea14d2d03af577500e0256f0a1f33f441
Francis Ong <francis.ong25@gmail.com>
2022-01-13 19:06:04 +0800

    Change button to dropdown (events page-admin)

--- Portal/admin/events.php
+++ Portal/admin/events.php
@@ -126,8 +126,17 @@ include 'includes/header.php';
                                               <td><?php echo (new Datetime($row['event_from']))->format('h:i a').' - '.(new Datetime($row['event_to']))->format('h:i a'); ?></td>
                                               <td><?php echo $row['event_venue']; ?></td>
                                               <td>
-                                                  <button class="btn btn-success btn-sm edit btn-round" data-id="<?php echo $row['reference_id']; ?>"><i class="fa fa-edit"></i> Edit</button>
-                                                  <button class="btn btn-danger btn-sm delete btn-round" data-id="<?php echo $row['reference_id']; ?>"><i class="fa fa-trash"></i> Delete</button>
+
+                                                  <button type="button" class="btn btn-default btn-sm btn-flat border-success wave-effect dropdown-toggle" data-toggle="dropdown" aria-expanded="true">Action
+                                                  </button>
+
+                                                  <div class="dropdown-menu" style="">
+                                                    <div class="dropdown-divider"></div>
+                                                    <a class="dropdown-item edit" href="javascript:void(0)" data-id="<?php echo $row['reference_id'] ?>"><i class="fa fa-edit"></i>Edit</a>
+                                                    <div class="dropdown-divider"></div>
+                                                    <a class="dropdown-item delete" href="javascript:void(0)" data-id="<?php echo $row['reference_id'] ?>"><i class="fa fa-trash"></i>Delete</a>
+                                                  </div>
+
                                               </td>
                                             </tr>
                                         <?php
