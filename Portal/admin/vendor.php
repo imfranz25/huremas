f1e30aec0af95ad22cb29d4623ed26c5a43a3754
Francis Ong <francis.ong25@gmail.com>
2022-01-17 11:47:34 +0800

    Relocate add/delete btn and change action to drop down (vendor-admin)

--- Portal/admin/vendor.php
+++ Portal/admin/vendor.php
@@ -1,18 +1,28 @@
 
-<button type="button" class="btn btn-mat waves-effect waves-light btn-success" data-toggle="modal" data-target="#vendorNew"><i class="fa fa-plus"></i>New</button>
-<button type="button" class="btn btn-mat waves-effect waves-light btn-danger" data-toggle="modal" id="deleteAllvendor"><i class="fa fa-trash"></i>Delete</button>
 
                             <br>
                             <div class="card">
                             <div class="card-header">
-                                                <h5>Vendor List</h5>
+                                                <!-- <h5>Vendor List</h5>
                                                 <div class="card-header-right">
                                                     <ul class="list-unstyled card-option">
                                                         <li><i class="fa fa fa-wrench open-card-option"></i></li>
                                                         <li><i class="fa fa-window-maximize full-card"></i></li>
                                                         <li><i class="fa fa-refresh reload-card"></i></li>
                                                     </ul>
-                                                </div>
+                                                </div> -->
+
+
+                              <h5>
+                                <a type="button" class="btn btn-mat waves-effect waves-light btn-default">Vendor List</a>
+                              </h5>
+                              <button type="button" class="btn btn-mat waves-effect waves-light btn-danger float-right" data-toggle="modal" id="deleteAllvendor"><i class="fa fa-trash"></i>Delete</button>
+                              <button type="button" class="btn btn-mat waves-effect waves-light btn-success float-right mx-2" data-toggle="modal" data-target="#vendorNew"><i class="fa fa-plus"></i>New</button>
+                                
+
+
+
+
                                             </div>
                             <div class="box-body">
                             <div class="card-block table-border-style">
@@ -39,30 +49,38 @@
                                             $sql = "SELECT * FROM deduction_vendor";
                                             $query = $conn->query($sql);
                                             while($row = $query->fetch_assoc()){
-                                            echo "
+                                                ?>
+
                                                 <tr>
                                                     <td>
                                                         <div class='custom-control custom-checkbox m-0 p-0'>
-                                                          <input type='checkbox' class='custom-control-input toggle-check' data-id='".$row['id']."' id='v".$row['id']."' />
-                                                          <label class='custom-control-label d-flex align-items-center' for='v".$row['id']."'>&nbsp;</label>
+                                                          <input type='checkbox' class='custom-control-input toggle-check' data-id='<?php echo $row['id']; ?>' id='<?php echo 'v'.$row['id']; ?>' />
+                                                          <label class='custom-control-label d-flex align-items-center' for='<?php echo 'v'.$row['id']; ?>'>&nbsp;</label>
                                                         </div>
                                                     </td>
-                                                    <td>".$row['vendor_code']."</td>
-                                                    <td>".$row['vendor_name']."</td>
+                                                    <td><?php echo $row['vendor_code']; ?></td>
+                                                    <td><?php echo $row['vendor_name']; ?></td>
                                                     <td style='overflow: hidden;white-space: nowrap;text-overflow: ellipsis;max-width: 290px;'>
-                                                    <a href='#view_desc' data-toggle='modal' class='pull-right desc' data-id='".$row['id']."'><span class='fa fa-eye ml-5'></span></a>
-                                                    
-                                                    ".$row['vendor_details']."
+                                                    <a href='#view_desc' data-toggle='modal' class='pull-right desc' data-id='<?php echo $row['id']; ?>'><span class='fa fa-eye ml-5'></span></a><?php echo $row['vendor_details']; ?>
                                                     </td>
-                                                    <td>".$row['vendor_type']."</td>
+                                                    <td><?php echo $row['vendor_type']; ?></td>
                                                     <td>
-                                                      <button class='btn btn-success btn-sm editvendor btn-round' data-id='".$row['id']."'><i class='fa fa-edit'></i> Edit</button>
-                                                      <button class='btn btn-danger btn-sm deletevendor btn-round' data-id='".$row['id']."'><i class='fa fa-trash'></i> Delete</button>
+
+
+
+                                                      <button type="button" class="btn btn-default btn-sm btn-flat border-success wave-effect dropdown-toggle" data-toggle="dropdown" aria-expanded="true">Action
+                                                    </button>
+
+                                                    <div class="dropdown-menu" style="">
+                                                      <a class="dropdown-item editvendor" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><i class="fa fa-edit"></i>Edit</a>
+                                                      <div class="dropdown-divider"></div>
+                                                      <a class="dropdown-item deletevendor text-danger" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><i class="fa fa-trash"></i>Delete</a>
+                                                    </div>
+
+
                                                     </td>
                                                 </tr>
-                                            ";
-                                            }
-                                        ?>
+                                           <?php } ?>
                                         </tbody>
                                 </table>
                                     </div>
