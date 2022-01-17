ec4de2e344334f6fbcdeabd52e80cfa6357eab9b
Francis Ong <francis.ong25@gmail.com>
2022-01-17 11:55:41 +0800

    Relocate add/delete btn and change action to drop down (deduction-admin)

--- Portal/admin/deduction_type.php
+++ Portal/admin/deduction_type.php
@@ -1,18 +1,23 @@
 
-<button type="button" class="btn btn-mat waves-effect waves-light btn-success" id="add_dtype" data-toggle="modal" data-target="#deducNew"><i class="fa fa-plus"></i>New</button>
-<button type="button" class="btn btn-mat waves-effect waves-light btn-danger" data-toggle="modal" id="deleteAlldeduc"><i class="fa fa-trash"></i>Delete</button>
 
                             <br>
                             <div class="card">
                             <div class="card-header">
-                                                <h5>Deduction Type List</h5>
+                                <h5>
+                                    <a type="button" class="btn btn-mat waves-effect waves-light btn-default">Deduction Type List</a>
+                                </h5>
+                                <button type="button" class="btn btn-mat waves-effect waves-light btn-danger float-right" data-toggle="modal" id="deleteAlldeduc"><i class="fa fa-trash"></i>Delete</button>
+                                <button type="button" class="btn btn-mat waves-effect waves-light btn-success float-right mx-2" id="add_dtype" data-toggle="modal" data-target="#deducNew"><i class="fa fa-plus"></i>New</button>
+
+
+                                                <!-- <h5>Deduction Type List</h5>
                                                 <div class="card-header-right">
                                                     <ul class="list-unstyled card-option">
                                                         <li><i class="fa fa fa-wrench open-card-option"></i></li>
                                                         <li><i class="fa fa-window-maximize full-card"></i></li>
                                                         <li><i class="fa fa-refresh reload-card"></i></li>
                                                     </ul>
-                                                </div>
+                                                </div> -->
                                             </div>
                             <div class="box-body">
                             <div class="card-block table-border-style">
@@ -43,26 +48,34 @@
                                                     $row['deduction_type'] =="Hour Amount") {
                                                     $amt_rate = "&#8369; ".number_format($row['amount_rate'],2);
                                                 }else{$amt_rate = $row['amount_rate']."%";}
-                                            echo "
+                                            ?>
                                                 <tr>
                                                     <td>
                                                         <div class='custom-control custom-checkbox m-0 p-0'>
-                                                          <input type='checkbox' class='custom-control-input toggle-check' data-id='".$row['did']."' id='d".$row['did']."' />
-                                                          <label class='custom-control-label d-flex align-items-center' for='d".$row['did']."'>&nbsp;</label>
+                                                          <input type='checkbox' class='custom-control-input toggle-check' data-id='<?php echo $row['did']; ?>' id='<?php echo 'd'.$row['did']; ?>' />
+                                                          <label class='custom-control-label d-flex align-items-center' for='<?php echo 'd'.$row['did']; ?>'>&nbsp;</label>
                                                         </div>
                                                     </td>
-                                                    <td>".$row['deduction_code']."</td>
-                                                    <td>".$row['deduction_name']."</td>
-                                                    <td>".$row['vendor_name']."</td>
-                                                    <td class='text-center'>".$amt_rate."</td>
+                                                    <td><?php echo $row['deduction_code']; ?></td>
+                                                    <td><?php echo $row['deduction_name']; ?></td>
+                                                    <td><?php echo $row['vendor_name']; ?></td>
+                                                    <td class='text-center'><?php echo $amt_rate; ?></td>
                                                     <td>
-                                                      <button class='btn btn-success btn-sm editdeduc btn-round' data-id='".$row['did']."'><i class='fa fa-edit'></i> Edit</button>
-                                                      <button class='btn btn-danger btn-sm deletededuc btn-round' data-id='".$row['did']."'><i class='fa fa-trash'></i> Delete</button>
+
+
+                                                      <button type="button" class="btn btn-default btn-sm btn-flat border-success wave-effect dropdown-toggle" data-toggle="dropdown" aria-expanded="true">Action
+                                                        </button>
+
+                                                        <div class="dropdown-menu" style="">
+                                                          <a class="dropdown-item editdeduc" href="javascript:void(0)" data-id="<?php echo $row['did'] ?>"><i class="fa fa-edit"></i>Edit</a>
+                                                          <div class="dropdown-divider"></div>
+                                                          <a class="dropdown-item deletededuc text-danger" href="javascript:void(0)" data-id="<?php echo $row['did'] ?>"><i class="fa fa-trash"></i>Delete</a>
+                                                        </div>
+
+
                                                     </td>
                                                 </tr>
-                                            ";
-                                            }
-                                        ?>
+                                            <?php } ?>
                                         </tbody>
                                 </table>
                                     </div>
