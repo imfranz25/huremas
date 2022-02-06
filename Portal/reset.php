4c74ae2d12179a9f10e38557c8e5cd9af7fd98a5
Francis Ong <francis.ong25@gmail.com>
2022-02-07 00:41:02 +0800

    apply sql prepare stmt in reset password, adjust tabs 2 spaces

--- Portal/reset.php
+++ Portal/reset.php
@@ -1,64 +1,62 @@
 <?php
-session_start();
-	include 'includes/conn.php';
-	 include 'sendEmail.php';
+
+	session_start();
+	require_once 'includes/path.php';
+	require_once $_SERVER['DOCUMENT_ROOT'].$global_link.'/Database/conn.php';
+	require_once 'sendEmail.php';
 
 	if(isset($_POST['reset'])){
         
+		//prepared statement
+		$stmt = $conn->prepare("SELECT * FROM admin a LEFT JOIN employees e 
+														ON a.employee_id=e.employee_id WHERE username = ? ");
+		$stmt->bind_param('s',$username);
+
+		//get values
 		$username = $_POST['username'];
 		$email = $_POST['email'];
 
-		$sql = "SELECT * FROM admin a left join employees e on a.employee_id=e.employee_id WHERE username = '$username'";
+		//execute
+		$stmt->execute();
+		$result = $stmt->get_result();
 
-		$query = $conn->query($sql);
-
-		if($query->num_rows < 1){
-            $_SESSION['error'] = 'Cannot find account with the username';
+		if($result->num_rows < 1){
+      $_SESSION['error'] = 'Cannot find account with the username';
 		}
 		else{
-			$row = $query->fetch_assoc();
+			$row = $result->fetch_assoc(); // get infos
 			if($email==$row['email']){
-				$id = $row['employee_id'];
-				$check_archive = "SELECT employee_archive FROM employees WHERE employee_id = '$id'";
+				$id = $row['employee_id']; //get id and check if the employee is already archived
+				$check_archive = "SELECT employee_archive FROM employees 
+													WHERE employee_id = '$id'";
 				$check_query = $conn->query($check_archive);
 				// CONTINUE TO EMPLOYEE PAGE -> EMPLOYEE IS NOT IN ARCHIVED
 				if ($check_query->num_rows > 0 ) {
-				    
-				    
-
+				  //fetch values
 					$gmail = $row['email'];
-				    $username = $row['username'];
-				    $default=$row['default_password'];
+			    $username = $row['username'];
+			    $default=$row['default_password'];
+			    $hashed_password = password_hash($row['default_password'], PASSWORD_DEFAULT);
+			    $sql1 = "UPDATE admin SET password = '$hashed_password' WHERE employee_id = '$id' ";
+			    $conn->query($sql1);
+			    //set email
+			    $subject="Account Password Reset";
+			    $message = "Hello!,<br><br>Your account password has been reset  <br>Username: ".$username."<br>Default Password: ".$default." <br><br>Please change your password immediately!";
+			    $res= sendEmail($gmail,$subject,$message);
+			    $_SESSION['success'] = $res;
 				    
-				    $hashed_password = password_hash($row['default_password'], PASSWORD_DEFAULT);
-				    
-				    $sql1 = "UPDATE admin SET password = '$hashed_password' WHERE employee_id = '$id' ";
-				    $conn->query($sql1);
-				    
-				    $subject="Account Password Reset";
-				    $message = "Hello!,<br><br>Your account password has been reset  <br>Username: ".$username."<br>Default Password: ".$default." <br><br>Please change your password immediately!";
-				    
-				    $res= sendEmail($gmail,$subject,$message);
-				    
-				    $_SESSION['success'] = $res;
-				    
-
 				}else{
 					$_SESSION['error'] = 'Sorry your account is not active at the moment, please contact your administrator';
 				}
-
-			}
-			else{
-                $_SESSION['error'] = 'Incorrect email';
+			}else{
+        $_SESSION['error'] = 'Incorrect email';
 			}
 		}
 		
-	}
-	else{
-        $_SESSION['error'] = 'Fill up the form first';
+	}else{
+    $_SESSION['error'] = 'Fill up the form first';
 	}
     
-    header("location: forgot.php");
-
+  header("location: forgot.php");
 
 ?>
