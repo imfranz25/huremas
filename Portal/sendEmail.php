7aac76e592b9da12848b3858c19d9a628ce43f38
Francis Ong <francis.ong25@gmail.com>
2022-02-07 00:42:20 +0800

    adjust tab space to 2

--- Portal/sendEmail.php
+++ Portal/sendEmail.php
@@ -1,40 +1,39 @@
 <?php
-require_once ("PHPMailer/autoload.php");
+
+  require_once ("PHPMailer/autoload.php");
 
 
-function sendEmail($gmail,$subject,$message){
+  function sendEmail($gmail,$subject,$message){
 
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-$mail->isSMTP();
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    $mail->isSMTP();
 
-$mail->SMTPDebug = 0;
-$mail->SMTPAuth = true; 
-$mail->SMTPSecure = 'ssl'; 
+    $mail->SMTPDebug = 0;
+    $mail->SMTPAuth = true; 
+    $mail->SMTPSecure = 'ssl'; 
 
-$mail->Host = 'smtp.gmail.com';
-$mail->Port = '465';
-$mail->isHTML();
-$mail->Username = 'dailysurg1001@gmail.com';
-$mail->Password = 'frewgod2956';
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = '465';
+    $mail->isHTML();
+    $mail->Username = 'dailysurg1001@gmail.com';
+    $mail->Password = 'frewgod2956';
 
-$mail->SetFrom('syratech@huremas-cvsuic.online');
-$mail->Subject = $subject;
-$mail->Body = $message;
-$mail->AddAddress($gmail);
-$mail->Send();
+    $mail->SetFrom('syratech@huremas-cvsuic.online');
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+    $mail->AddAddress($gmail);
+    $mail->Send();
 
-$res="";
-if($mail->Send()){
-    $res = "An e-mail has been sent to ".$gmail." ";
-}else{
-    $res = "Failed to send an e-mail to ".$gmail." ";
-    
-}
+    $res="";
+    if($mail->Send()){
+        $res = "An e-mail has been sent to ".$gmail." ";
+    }else{
+        $res = "Failed to send an e-mail to ".$gmail." ";
+    }
 
-return $res;
+    return $res;
 
-
-}
+  }
 
 ?>
 
