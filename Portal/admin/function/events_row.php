6673d38cc92bb152965a6b942357bb06f207d88f
Francis Ong <francis.ong25@gmail.com>
2022-02-08 09:46:26 +0800

    apply prepared stmt events row

--- Portal/admin/function/events_row.php
+++ Portal/admin/function/events_row.php
@@ -1,28 +1,36 @@
 <?php 
-	require_once '../../includes/path.php';
+  require_once '../../includes/path.php';
 	require_once '../includes/session.php';
 
-	if(isset($_POST['id'])){
-		$id = $_POST['id'];
-		$sql = "SELECT * FROM events WHERE reference_id = '$id'";
-		$query = $conn->query($sql);
-		$row = $query->fetch_assoc();
+  //prepared stmt events select
+  $get_events = $conn->prepare("SELECT * FROM events WHERE reference_id = ? ");
+  $get_events->bind_param('s',$id);
 
+	if(isset($_POST['id'])){
+    global $get_events;
+    
+		$id = $_POST['id'];
+    $get_events->execute();
+    $result = $get_events->get_result();
+    $row = $result->fetch_assoc();
 		echo json_encode($row);
+
 	}else if(isset($_POST['ids'])){
-		// initilization shit
+    global $get_events;
+
+		// initilization 
 		$ids =$_POST['ids'];
 		$event_name = array();
-		$stmt = $conn->prepare("SELECT * FROM events WHERE reference_id = ? ");
-		$stmt->bind_param("s", $id);
 
+    //get all rows and push it to array -> encode as JSON
 		for ($i=0; $i < count($ids); $i++) { 
 			$id = $ids[$i];
-			$stmt->execute();
-			$result = $stmt->get_result();
+			$get_events->execute();
+			$result = $get_events->get_result();
 			$row = $result->fetch_assoc();
 			array_push($event_name, $row['event_name']);
 		}
 		echo json_encode($event_name);
+
 	}
 ?>
