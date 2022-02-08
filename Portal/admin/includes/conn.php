919bd2ddc6312062e251049141cd24ad21395789
Francis Ong <francis.ong25@gmail.com>
2022-02-08 13:21:19 +0800

    delete conn.php in includes/ , adjust tab spaces in scripts

--- Portal/admin/includes/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$conn = new mysqli('localhost', 'u418351397_huremas', 'Syracrus99', 'u418351397_huremas');
-
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-	
-?>
