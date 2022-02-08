89be980b0834302b47225b3ea9c6e3b99af5d919
Francis Ong <francis.ong25@gmail.com>
2022-02-08 13:30:39 +0800

    adjust tab spaces to 2

--- Portal/admin/includes/events_modal.php
+++ Portal/admin/includes/events_modal.php
@@ -1,147 +1,124 @@
 
-
 <!-- Create Event -->
 <div class="modal fade" id="addEvents">
-    <div class="modal-dialog modal-md">
-        <div class="modal-content">
-          	<div class="modal-header">
-            	
-            	<h4 class="modal-title"><b>Create Event</b></h4>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-          	</div>
-          	<div class="modal-body">
-
-            <form class="form-horizontal" method="POST" action="function/events_add.php" enctype="multipart/form-data" >
-
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label req">Date</label>
-                  <div class="col-sm-10">
-                    <input type="date" class="form-control border border-secondary" min="<?php echo date('Y-m-d'); ?>" name="event_date" required />
-                  </div>
-
-                </div>
-
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label req">Image</label>
-                  <div class="col-sm-10">
-                    <input type="file" name="display" class="form-control border border-secondary" accept="image/*" onchange="check_image(this)"  required="" >
-                  </div>
-                </div>
-
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label req">Name</label>
-                  <div class="col-sm-10">
-                    <input type="text" name="event_name" class="form-control border border-secondary"  required="" autocomplete="off" >
-                  </div>
-                </div>
-
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label req">Time</label>
-                  <div class="col-sm-5">
-                    <input type="time" name="event_from" class="form-control border border-secondary event_from"  required="" placeholder="From">
-                  </div>
-                  <div class="col-sm-5">
-                    <input type="time" name="event_to" class="form-control border border-secondary event_to"  required="" placeholder="To">
-                  </div>
-                </div>
-
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label req">Venue</label>
-                  <div class="col-sm-10">
-                    <input type="text" name="venue" class="form-control border border-secondary"  required="" autocomplete="off"  >
-                  </div>
-                </div>
-
-
-          	</div>
-          	<div class="modal-footer">
-            	<button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
-            	<button type="submit" class="btn btn-success btn-flat" name="add"><i class="fa fa-save"></i> Post</button>
-            	</form>
-          	</div>
+  <div class="modal-dialog modal-md">
+    <div class="modal-content">
+    	<div class="modal-header">
+      	<h4 class="modal-title"><b>Create Event</b></h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+    	</div>
+      <form class="form-horizontal" method="POST" action="function/events_add.php" enctype="multipart/form-data" >
+      	<div class="modal-body">
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label req">Date</label>
+            <div class="col-sm-10">
+              <input type="date" class="form-control border border-secondary" min="<?php echo date('Y-m-d'); ?>" name="event_date" required />
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label req">Image</label>
+            <div class="col-sm-10">
+              <input type="file" name="display" class="form-control border border-secondary" accept="image/*" onchange="check_image(this)"  required="" >
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label req">Name</label>
+            <div class="col-sm-10">
+              <input type="text" name="event_name" class="form-control border border-secondary"  required="" autocomplete="off" >
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label req">Time</label>
+            <div class="col-sm-5">
+              <input type="time" name="event_from" class="form-control border border-secondary event_from"  required="" placeholder="From">
+            </div>
+            <div class="col-sm-5">
+              <input type="time" name="event_to" class="form-control border border-secondary event_to"  required="" placeholder="To">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label req">Venue</label>
+            <div class="col-sm-10">
+              <input type="text" name="venue" class="form-control border border-secondary"  required="" autocomplete="off"  >
+            </div>
+          </div>
         </div>
+      	<div class="modal-footer">
+        	<button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+        	<button type="submit" class="btn btn-success btn-flat" name="add"><i class="fa fa-save"></i> Post</button>
+      	</div>
+      </form>
+      <!-- End of Form -->
     </div>
+  </div>
 </div>
-
-
 
 <!-- Edit Event -->
 <div class="modal fade" id="editEvents">
-    <div class="modal-dialog modal-md">
-        <div class="modal-content">
-            <div class="modal-header">
-              
-              <h4 class="modal-title"><b>Edit Event</b></h4>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
+  <div class="modal-dialog modal-md">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title"><b>Edit Event</b></h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form class="form-horizontal" method="POST" action="function/events_edit.php" enctype="multipart/form-data" >
+        <div class="modal-body">
+          <input type="hidden" name="reference_id" class="reference_id">
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label req">Date</label>
+            <div class="col-sm-10">
+              <input type="date" class="form-control border border-secondary event_date" name="event_date" required />
             </div>
-            <div class="modal-body">
-
-            <form class="form-horizontal" method="POST" action="function/events_edit.php" enctype="multipart/form-data" >
-
-              <input type="hidden" name="reference_id" class="reference_id">
-
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label req">Date</label>
-                  <div class="col-sm-10">
-                    <input type="date" class="form-control border border-secondary event_date" name="event_date" required />
-                  </div>
-
-                </div>
-
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label">Image</label>
-                  <div class="col-sm-10">
-                    <label class="col-form-label"><a class="event_image" target="_blank" href=""></a></label>
-                    <input type="file" name="display" class="form-control border border-secondary" accept="image/*" onchange="check_image(this)" >
-                    <!-- INFO FOR FILE -->
-                    <div class="input-group-append">
-                      <span class="input-group-text text-info" >
-                        <i class="fa fa-info-circle"></i>
-                        <strong>Leave upload file to default if you dont wish to change the display image</strong>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label req">Name</label>
-                  <div class="col-sm-10">
-                    <input type="text" name="event_name" class="form-control border border-secondary event_name"  required="" autocomplete="off" >
-                  </div>
-                </div>
-
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label req">Time</label>
-                  <div class="col-sm-5">
-                    <input type="time" name="event_from" class="form-control border border-secondary from event_timefrom event_from" required="" placeholder="From">
-                  </div>
-                  <div class="col-sm-5">
-                    <input type="time" name="event_to" class="form-control border border-secondary to event_timeto event_to" required="" placeholder="To">
-                  </div>
-                </div>
-
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label req">Venue</label>
-                  <div class="col-sm-10">
-                    <input type="text" name="venue" class="form-control border border-secondary event_venue"  required="" autocomplete="off"  >
-                  </div>
-                </div>
-
-
+          </div>
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Image</label>
+            <div class="col-sm-10">
+              <label class="col-form-label"><a class="event_image" target="_blank" href=""></a></label>
+              <input type="file" name="display" class="form-control border border-secondary" accept="image/*" onchange="check_image(this)" >
+              <!-- INFO FOR FILE -->
+              <div class="input-group-append">
+                <span class="input-group-text text-info" >
+                  <i class="fa fa-info-circle"></i>
+                  <strong>Leave upload file to default if you dont wish to change the display image</strong>
+                </span>
+              </div>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
-              <button type="submit" class="btn btn-success btn-flat" name="edit"><i class="fa fa-save"></i> Update Event</button>
-              </form>
+          </div>
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label req">Name</label>
+            <div class="col-sm-10">
+              <input type="text" name="event_name" class="form-control border border-secondary event_name"  required="" autocomplete="off" >
             </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label req">Time</label>
+            <div class="col-sm-5">
+              <input type="time" name="event_from" class="form-control border border-secondary from event_timefrom event_from" required="" placeholder="From">
+            </div>
+            <div class="col-sm-5">
+              <input type="time" name="event_to" class="form-control border border-secondary to event_timeto event_to" required="" placeholder="To">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label req">Venue</label>
+            <div class="col-sm-10">
+              <input type="text" name="venue" class="form-control border border-secondary event_venue"  required="" autocomplete="off"  >
+            </div>
+          </div>
         </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+          <button type="submit" class="btn btn-success btn-flat" name="edit"><i class="fa fa-save"></i> Update Event</button>
+        </div>
+      </form>
+      <!-- End of Form -->
     </div>
+  </div>
 </div>
-
-
-
 
 <!-- Delete Event -->
 <div class="modal fade" id="eventsDelete">
@@ -154,8 +131,8 @@
         </button>
       </div>
       <form action="function/events_delete.php" method="POST">
-        <input type="hidden" name="id" class="reference_id">
         <div class="modal-body">
+          <input type="hidden" name="id" class="reference_id">
           <div class="text-center">
             <label>Are you sure you want to this event(s)?</label>
             <h2 id="del_events" class="bold"></h2>
@@ -178,13 +155,14 @@
           <button type="submit" class="btn btn-danger btn-flat" name="delete"><i class="fa fa-trash"></i> Delete</button>
         </div>
       </form>
+      <!-- End of Form -->
     </div>
   </div>
 </div>
 
+<!-- ==========================EVENT REQUEST====================================== -->
 
-
-<!-- Event Request -->
+<!-- Event Request Modal-->
 <div class="modal fade" id="eRequest">
   <div class="modal-dialog modal-xl">
     <div class="modal-content">
@@ -195,7 +173,6 @@
         </button>
       </div>
       <div class="modal-body">
-
         <!-- Card Block -->
         <div class="card">
           <div class="card-block mb-0">
@@ -215,7 +192,6 @@
                   <div class="slide"></div>
                 </li>
               </ul>
-
               <!-- Tab panes -->
               <div class="tab-content">
                 <div class="tab-pane active" id="Ereq" role="tabpanel">
@@ -228,25 +204,17 @@
                   <br><?php include 'event_rejected.php'; ?>   
                 </div>
               </div>
-
             </div>
           </div>
           <!-- Main-body end -->
         </div>
-
       </div>
-
       <div class="modal-footer">
         <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
       </div>
-
     </div>
   </div>
 </div>
-
-
-
-
 
 <!-- Request reView -->
 <div class="modal fade" id="reviewRequest" style="background:rgba(0,0,0,.7)">
@@ -259,8 +227,8 @@
         </button>
       </div>
       <form method="POST" action="function/event_evaluate.php">
-        <input type="hidden" name="reference_id" class="ereq_id">
         <div class="modal-body">
+          <input type="hidden" name="reference_id" class="ereq_id">
           <div class="form-group row">
             <label class="col-sm-2 col-form-label">Date</label>
             <div class="col-sm-10">
@@ -294,14 +262,12 @@
               <input type="text" class="form-control border border-secondary event_venue"  readonly >
             </div>
           </div>
-
           <div class="form-group row">
             <label class="col-sm-2 col-form-label">Details</label>
             <div class="col-sm-10">
               <textarea readonly class="form-control border border-secondary event_details" rows="8"></textarea>
             </div>
           </div>
-
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
@@ -309,12 +275,10 @@
           <button type="submit" class="btn btn-success btn-flat pull-left" name="approve" ><i class="fa fa-thumbs-o-up"></i> Approve</button>
         </div>
       </form>
+      <!-- End of Form -->
     </div>
   </div>
 </div>
-
-
-
 
 
 <!-- Request View -->
@@ -361,14 +325,12 @@
             <input type="text" class="form-control border border-secondary event_venue"  readonly >
           </div>
         </div>
-
         <div class="form-group row">
           <label class="col-sm-2 col-form-label">Details</label>
           <div class="col-sm-10">
             <textarea readonly class="form-control border border-secondary event_details" rows="8"></textarea>
           </div>
         </div>
-
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
@@ -376,12 +338,4 @@
     </div>
   </div>
 </div>
-
-
-
-
-
-
-
-
 
