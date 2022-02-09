16a734d9c76a8b3962ad97b065e47f453bd3974b
Francis Ong <francis.ong25@gmail.com>
2022-02-09 10:47:36 +0800

    adjsut tab space indention

--- Portal/admin/includes/header.php
+++ Portal/admin/includes/header.php
@@ -2,64 +2,46 @@
 <html lang="en">
 
 <head>
-    <title><?php echo (isset($title)) ? $title." |" : ""; ?> HUREMAS - CvSU Imus</title>
-    <!-- HTML5 Shim and Respond.js IE10 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 10]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-      <![endif]-->
-      <!-- Meta -->
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="description" content="MHumarn Resource Management System - CVSU - IMUS />
-      <meta name="keywords" content="cvsu, hr, human resource, huremas" />
-      <meta name="author" content="Francis Ong, Ran Bellon" />
-      <!-- Favicon icon -->
-      <link rel="icon" href="../assets/images/Tabicon.ico" type="image/x-icon">
+  <title><?php echo (isset($title)) ? $title." |" : ""; ?> HUREMAS - CvSU Imus</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="MHumarn Resource Management System - CVSU - IMUS" />
+    <meta name="keywords" content="cvsu, hr, human resource, huremas" />
+    <meta name="author" content="Francis Ong, Ran Bellon" />
+    <!-- Favicon icon -->
+    <link rel="icon" href="../assets/images/Tabicon.ico" type="image/x-icon">
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
-
-    
     <!-- waves.css -->
     <link rel="stylesheet" href="../assets/pages/waves/css/waves.min.css" type="text/css" media="all">
-      <!-- Required Fremwork -->
-      <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap/css/bootstrap.min.css">
-      <!-- waves.css -->
-      <link rel="stylesheet" href="../assets/pages/waves/css/waves.min.css" type="text/css" media="all">
-      <!-- themify icon -->
-      <link rel="stylesheet" type="text/css" href="../assets/icon/themify-icons/themify-icons.css">
-      <!-- Font Awesome -->
-      <link rel="stylesheet" type="text/css" href="../assets/icon/font-awesome/css/font-awesome.min.css">
-      <!-- scrollbar.css -->
-      <link rel="stylesheet" type="text/css" href="../assets/css/jquery.mCustomScrollbar.css">
-        <!-- am chart export.css -->
-        <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
-      <!-- Style.css -->
-      <link rel="stylesheet" type="text/css" href="../assets/css/style.css?<?php echo time(); ?>">
-
-      <link rel="stylesheet" href="../assets/css/modals.css">
-
-      <!-- Calendar -->
-      <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+    <!-- Required Fremwork -->
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap/css/bootstrap.min.css">
+    <!-- waves.css -->
+    <link rel="stylesheet" href="../assets/pages/waves/css/waves.min.css" type="text/css" media="all">
+    <!-- themify icon -->
+    <link rel="stylesheet" type="text/css" href="../assets/icon/themify-icons/themify-icons.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" type="text/css" href="../assets/icon/font-awesome/css/font-awesome.min.css">
+    <!-- scrollbar.css -->
+    <link rel="stylesheet" type="text/css" href="../assets/css/jquery.mCustomScrollbar.css">
+    <!-- am chart export.css -->
+    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+    <!-- Style.css -->
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="../assets/css/modals.css">
+    <!-- Calendar -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
     <link rel="stylesheet" href="../calendar/css/demo.css"/>
     <link rel="stylesheet" href="../calendar/css/theme1.css"/>
-
-      <!-- table data search etc -->
+    <!-- table data search etc -->
     <link href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css" rel="stylesheet">
-
     <!-- summer note -->
     <link rel="stylesheet" href="../assets/summernote/summernote.css">
-
-
-
-
     <style>
       .req:after{
         content: ' *';
         color: red;
       }
     </style>
-      
   </head>
