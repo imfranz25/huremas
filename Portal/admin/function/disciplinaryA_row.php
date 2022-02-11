144fc14b577f31032e70473e0a44c00e3d7cdcdf
Francis Ong <francis.ong25@gmail.com>
2022-02-11 22:27:48 +0800

    add semicolon cuz i forgot hahahaha

--- Portal/admin/function/disciplinaryA_row.php
+++ Portal/admin/function/disciplinaryA_row.php
@@ -6,7 +6,7 @@
 
 		// select prepared stmt
 		$sql = $conn->prepare("SELECT * FROM disciplinary_action LEFT JOIN disciplinary_category ON disciplinary_category.id=disciplinary_action.reason LEFT JOIN employees ON  employees.employee_id=disciplinary_action.employee_id  WHERE reference_id = ? ");
-    $sql->bind_param('s',$id)
+    $sql->bind_param('s',$id);
     //get id
     $id = $_POST['id'];
     $sql->execute();
