3191822f9a09ef5f9771af505757e693f624e632
Francis Ong <francis.ong25@gmail.com>
2022-04-15 13:10:36 +0800

    fix indention at js section

--- Portal/admin/new_evaluation.php
+++ Portal/admin/new_evaluation.php
@@ -1,5 +1,3 @@
-<?php
-?>
 <div class="col-lg-12">
 	<div class="card">
 		<div class="card-body">
@@ -11,8 +9,8 @@
 							<select name="employee_id" id="employee_id" class="form-control form-control-sm select2">
 								<option value=""></option>
 								<?php 
-								$employees = $conn->query("SELECT *,concat(lastname,', ',firstname,' ',middlename) as name FROM employees order by concat(lastname,', ',firstname,' ',middlename) asc");
-								while($row=$employees->fetch_assoc()):
+  								$employees = $conn->query("SELECT *,concat(lastname,', ',firstname,' ',middlename) as name FROM employees order by concat(lastname,', ',firstname,' ',middlename) asc");
+  								while($row=$employees->fetch_assoc()):
 								?>
 								<option value="<?php echo $row['employee_id'] ?>"><?php echo $row['name'] ?></option>
 								<?php endwhile; ?>
@@ -34,7 +32,7 @@
 									<label for="" class="control-label">Efficiency</label>
 									<select name="efficiency" id="efficiency" class="form-control form-control-sm" required>
 										<?php 
-										for($i = 5; $i >= 0;$i--):
+										  for($i = 5; $i >= 0;$i--):
 										?>
 										<option ><?php echo $i ?></option>
 										<?php endfor; ?>
@@ -44,7 +42,7 @@
 									<label for="" class="control-label">Timeliness</label>
 									<select name="timeliness" id="timeliness" class="form-control form-control-sm" required>
 										<?php 
-										for($i = 5; $i >= 0;$i--):
+										  for($i = 5; $i >= 0;$i--):
 										?>
 										<option ><?php echo $i ?></option>
 										<?php endfor; ?>
@@ -56,7 +54,7 @@
 									<label for="" class="control-label">Quality</label>
 									<select name="quality" id="quality" class="form-control form-control-sm" required>
 										<?php 
-										for($i = 5; $i >= 0;$i--):
+										  for($i = 5; $i >= 0;$i--):
 										?>
 										<option ><?php echo $i ?></option>
 										<?php endfor; ?>
@@ -66,7 +64,7 @@
 									<label for="" class="control-label">Accuracy</label>
 									<select name="accuracy" id="accuracy" class="form-control form-control-sm" required>
 										<?php 
-										for($i = 5; $i >= 0;$i--):
+										  for($i = 5; $i >= 0;$i--):
 										?>
 										<option ><?php echo $i ?></option>
 										<?php endfor; ?>
@@ -97,24 +95,23 @@
 </div>
 <div id="clone_progress" class="d-none">
 	<div class="post">
-              <div class="user-block">
-                <img class="img-circle img-bordered-sm avatar" src="" alt="user image" style='border:solid gray 2px ;padding:1px;max-width: 40px;height: auto;'>
-                <span class="username">
-                  <a href="#" class="nf"></a>
-                </span>
-                <span class="description">
-                	<span class="fa fa-calendar-day"></span>
-                	<span><b class="date"></b></span>
-            	</span>
-              </div>
-              <div class="pdesc">
-              
-              </div>
-
-              <p>
-              </p>
-        </div>
+    <div class="user-block">
+      <img class="img-circle img-bordered-sm avatar" src="" alt="user image" style='border:solid gray 2px ;padding:1px;max-width: 40px;height: auto;'>
+      <span class="username">
+        <a href="#" class="nf"></a>
+      </span>
+      <span class="description">
+      	<span class="fa fa-calendar-day"></span>
+      	<span><b class="date"></b></span>
+  	 </span>
+    </div>
+    <div class="pdesc">
+      <!-- Description here -->
+    </div>
+    <p></p>
+  </div>
 </div>
+
 <style>
 	img#cimg{
 		height: 15vh;
@@ -127,110 +124,112 @@
 		overflow: auto;
 	}
 </style>
+
 <script>
+
+  function displayImg(input,_this) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('#cimg').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  function update_emp(id){
+    $('#task_id').html('')
+    $.ajax({
+      url:'function/new_evaluation_row.php',
+      method:'POST',
+      data:{employee_id:id},
+      error:(err)=>{
+        alert_toast("An error occured",'error')
+        console.log(err)
+      },
+      success:function(resp){
+        resp = JSON.parse(resp)
+           var opt = $('<option value=""></option>')
+           if(Object.keys(resp).length > 0){
+              var oc = opt.clone()
+              $('#task_id').append(oc)
+            Object.keys(resp).map(k=>{
+              var oc = opt.clone()
+              oc.text(resp[k].task)
+              oc.attr('value',resp[k].id)
+              var task_id ="" ;
+              if(task_id == resp[k].id)
+                oc.attr('selected',true)
+              $('#task_id').append(oc)
+            })
+           }else{
+              $('#task_id').html('')
+            var oc = opt.clone()
+              oc.text("Employee is not assign to any task yet.")
+              oc.attr({'disabled':'disabled','selected':'selected'})
+              $('#task_id').append(oc)
+           }
+      },
+      complete:function(){
+        $('#task_id').select2({
+          placeholder:'Please select task here',
+          width:'100%'
+        })
+      }
+    })
+  }
+
+  function task_update(task_id){
+    $.ajax({
+      url:'function/get_progress_row.php',
+      method:'POST',
+      data:{task_id:task_id},
+      error:(err)=>{
+        alert_toast("An error occured",'error')
+        console.log(err)
+      },
+      success:function(resp){
+        if(resp && typeof JSON.parse(resp) === 'object'){
+          resp = JSON.parse(resp)
+          if(Object.keys(resp).length > 0){
+            $('#post-field').html('')
+            Object.keys(resp).map(k=>{
+              var _progress = $('#clone_progress .post').clone()
+              _progress.find('.pdesc').append(resp[k].progress)
+              _progress.find('.avatar').attr('src','../assets/images/'+resp[k].photo)
+              _progress.find('.nf').text(resp[k].uname)
+              _progress.find('.date').text(resp[k].date_created)
+
+              $('#post-field').append(_progress)
+            })
+            $('#ratings-field').show()
+          }else{
+            $('#ratings-field').hide()
+          }
+        }
+      },
+      complete:function(){
+      }
+    })
+  }
+
 	$(document).ready(function(){
+
 		if('<?php echo isset($id) ?>' == 1){
-			update_emp()
+			update_emp();
 		}
-	})
+
+    $('#employee_id').change(function(){
+    var id = $(this).val();
+    update_emp(id);
+    });
+
+    $('#task_id').change(function(){
+      var id = $(this).val();
+      task_update(id);
+    });
+
+	});
 	
-
-	function displayImg(input,_this) {
-	    if (input.files && input.files[0]) {
-	        var reader = new FileReader();
-	        reader.onload = function (e) {
-	        	$('#cimg').attr('src', e.target.result);
-	        }
-
-	        reader.readAsDataURL(input.files[0]);
-	    }
-	}
-
-	$('#employee_id').change(function(){
-		var id = $(this).val();
-		update_emp(id);
-	})
-	$('#task_id').change(function(){
-		var id = $(this).val();
-		task_update(id);
-	})
-
-	function update_emp(id){
- 		$('#task_id').html('')
-		$.ajax({
-			url:'function/new_evaluation_row.php',
-			method:'POST',
-			data:{employee_id:id},
-			error:(err)=>{
-				alert_toast("An error occured",'error')
-				console.log(err)
-			},
-			success:function(resp){
-				resp = JSON.parse(resp)
-					 var opt = $('<option value=""></option>')
-					 if(Object.keys(resp).length > 0){
-					 		var oc = opt.clone()
-					 		$('#task_id').append(oc)
-					 	Object.keys(resp).map(k=>{
-					 		var oc = opt.clone()
-					 		oc.text(resp[k].task)
-					 		oc.attr('value',resp[k].id)
-							var task_id ="" ;
-							if(task_id == resp[k].id)
-								oc.attr('selected',true)
-					 		$('#task_id').append(oc)
-					 	})
-					 }else{
-					 		$('#task_id').html('')
-					 	var oc = opt.clone()
-					 		oc.text("Employee is not assign to any task yet.")
-					 		oc.attr({'disabled':'disabled','selected':'selected'})
-					 		$('#task_id').append(oc)
-					 }
-			},
-			complete:function(){
-				$('#task_id').select2({
-					placeholder:'Please select task here',
-					width:'100%'
-				})
-			}
-		})
-	}
-
-	function task_update(task_id){
-
-			$.ajax({
-			url:'function/get_progress_row.php',
-			method:'POST',
-			data:{task_id:task_id},
-			error:(err)=>{
-				alert_toast("An error occured",'error')
-				console.log(err)
-			},
-			success:function(resp){
-				if(resp && typeof JSON.parse(resp) === 'object'){
-					resp = JSON.parse(resp)
-					if(Object.keys(resp).length > 0){
-						$('#post-field').html('')
-						Object.keys(resp).map(k=>{
-							var _progress = $('#clone_progress .post').clone()
-							_progress.find('.pdesc').append(resp[k].progress)
-							_progress.find('.avatar').attr('src','../assets/images/'+resp[k].photo)
-							_progress.find('.nf').text(resp[k].uname)
-							_progress.find('.date').text(resp[k].date_created)
-
-							$('#post-field').append(_progress)
-						})
-					 	$('#ratings-field').show()
-					}else{
-					 	$('#ratings-field').hide()
-					}
-				}
-			},
-			complete:function(){
-			}
-		})
-	}
-
 
 </script>
