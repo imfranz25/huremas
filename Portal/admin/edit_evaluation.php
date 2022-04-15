e1f29ef33c9a4034ff1671f15f39d81830f80c57
Francis Ong <francis.ong25@gmail.com>
2022-04-15 13:04:36 +0800

    fix js indntions to make it readable

--- Portal/admin/edit_evaluation.php
+++ Portal/admin/edit_evaluation.php
@@ -1,29 +1,25 @@
-<?php
-?>
+
 <div class="col-lg-12">
 	<div class="card">
 		<div class="card-body">
 			<h5 class="m-b-10">Edit Evaluation</h5><br>
 			<form action="function/evaluation_edit.php" id="manage_evaluation" method="POST">
-
 				<div class="row">
 					<div class="col-md-6 border-right">
 						<div class="form-group">
 							<label for="" class="control-label">Employee</label>
-								<?php 
+							<?php 
 								$rid = $_GET['id'];
 								$sql = $conn->query("SELECT r.*,concat(e.lastname,', ',e.firstname,' ',e.middlename) as name,t.task FROM ratings r left join employees e on r.employee_id= e.employee_id left join task t on r.task_id=t.id WHERE r.id='$rid'");
 								while($row=$sql->fetch_assoc()):
-								?>
-								<input type="text" class="form-control"  name="employee_id" id="employee_id" value="<?php echo $row['name'] ?>" readonly>
-
+							?>
+							<input type="text" class="form-control"  name="employee_id" id="employee_id" value="<?php echo $row['name'] ?>" readonly>
 						</div>
 						<div class="form-group">
 							<input type="hidden" name="" id="taskid" value="<?php echo $row['task_id']; ?>">
 							<input type="hidden" name="rid" value="<?php echo $rid; ?>">
 							<label for="" class="control-label">Task</label>
 							<input type="text" class="form-control"  name="task" id="task" value="<?php echo $row['task'] ?>" readonly>
-								
 						</div>
 						<div class="row " id="ratings-field">
 							<div class="col-md-12">
@@ -79,7 +75,6 @@
 								<textarea name="remarks" id="remarks" cols="30" rows="3" class="form-control"><?php echo $row['remarks']; ?></textarea>
 							</div>
 						</div>
-						
 					</div>
 					<div class="col-md-6">
 						<div id="post-field">
@@ -99,24 +94,23 @@
 </div>
 <div id="clone_progress" class="d-none">
 	<div class="post">
-              <div class="user-block">
-                <img class="img-circle img-bordered-sm avatar" src="" alt="user image" style='border:solid gray 2px ;padding:1px;max-width: 40px;height: auto;'>
-                <span class="username">
-                  <a href="#" class="nf"></a>
-                </span>
-                <span class="description">
-                	<span class="fa fa-calendar-day"></span>
-                	<span><b class="date"></b></span>
-            	</span>
-              </div>
-              <div class="pdesc">
-              
-              </div>
-
-              <p>
-              </p>
-        </div>
+    <div class="user-block">
+      <img class="img-circle img-bordered-sm avatar" src="" alt="user image" style='border:solid gray 2px ;padding:1px;max-width: 40px;height: auto;'>
+      <span class="username">
+        <a href="#" class="nf"></a>
+      </span>
+      <span class="description">
+      	<span class="fa fa-calendar-day"></span>
+      	<span><b class="date"></b></span>
+  	 </span>
+    </div>
+    <div class="pdesc">
+      <!-- Description Here -->
+    </div>
+    <p></p>
+  </div>
 </div>
+
 <style>
 	img#cimg{
 		height: 15vh;
@@ -129,65 +123,55 @@
 		overflow: auto;
 	}
 </style>
+
 <script>
+
+  function displayImg(input,_this) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('#cimg').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  function task_update(task_id){
+    $.ajax({
+      url:'function/get_progress_row.php',
+      method:'POST',
+      data:{task_id:task_id},
+      error:(err)=>{
+        alert_toast("An error occured",'error');
+      },
+      success:function(resp){
+        if(resp && typeof JSON.parse(resp) === 'object'){
+          resp = JSON.parse(resp)
+          if(Object.keys(resp).length > 0){
+            $('#post-field').html('')
+            Object.keys(resp).map(k=>{
+              var _progress = $('#clone_progress .post').clone()
+              _progress.find('.pdesc').append(resp[k].progress)
+              _progress.find('.avatar').attr('src','/Portal/admin/images/'+resp[k].photo)
+              _progress.find('.nf').text(resp[k].uname)
+              _progress.find('.date').text(resp[k].date_created)
+
+              $('#post-field').append(_progress)
+            })
+            $('#ratings-field').show()
+          }else{
+            $('#ratings-field').hide()
+          }
+        }
+      }
+    });
+  }
+
 	$(document).ready(function(){
 		if('<?php echo isset($rid) ?>'){
 			var id = $("#taskid").val();
 			task_update(id);
 		}
 	})
-	
-
-	function displayImg(input,_this) {
-	    if (input.files && input.files[0]) {
-	        var reader = new FileReader();
-	        reader.onload = function (e) {
-	        	$('#cimg').attr('src', e.target.result);
-	        }
-
-	        reader.readAsDataURL(input.files[0]);
-	    }
-	}
-
-
-
-	
-	
-
-	function task_update(task_id){
-
-			$.ajax({
-			url:'function/get_progress_row.php',
-			method:'POST',
-			data:{task_id:task_id},
-			error:(err)=>{
-				alert_toast("An error occured",'error')
-				console.log(err)
-			},
-			success:function(resp){
-				if(resp && typeof JSON.parse(resp) === 'object'){
-					resp = JSON.parse(resp)
-					if(Object.keys(resp).length > 0){
-						$('#post-field').html('')
-						Object.keys(resp).map(k=>{
-							var _progress = $('#clone_progress .post').clone()
-							_progress.find('.pdesc').append(resp[k].progress)
-							_progress.find('.avatar').attr('src','/Portal/admin/images/'+resp[k].photo)
-							_progress.find('.nf').text(resp[k].uname)
-							_progress.find('.date').text(resp[k].date_created)
-
-							$('#post-field').append(_progress)
-						})
-					 	$('#ratings-field').show()
-					}else{
-					 	$('#ratings-field').hide()
-					}
-				}
-			},
-			complete:function(){
-			}
-		})
-	}
-
 
 </script>
