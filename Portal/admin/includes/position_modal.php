1eb0356e470da0ead7f70580817a41b7b4838aaf
Francis Ong <francis.ong25@gmail.com>
2022-04-15 23:18:31 +0800

    add password field

--- Portal/admin/includes/position_modal.php
+++ Portal/admin/includes/position_modal.php
@@ -269,6 +269,13 @@
               <i class="fa fa-exclamation-circle mx-1" aria-hidden="true"></i>
               <label> Note: This process cannot be undone</label>
             </div>
+            <hr />
+            <div class="form-group row">
+              <label class="col-sm-3 col-form-label req">Password</label>
+              <div class="col-sm-9">
+                <input type="password" name="pass" class="form-control border border-secondary" required="" placeholder="Please enter your password for verification"  />
+              </div>
+            </div> 
           </div>
         </div>
         <div class="modal-footer">
