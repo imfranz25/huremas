515664e8c83c3977920600b5322ffe9c5a48e0d8
Francis Ong <francis.ong25@gmail.com>
2022-04-16 16:44:35 +0800

    add function in deleting attachment -> delete applicant record

--- Portal/admin/function/applicant_delete.php
+++ Portal/admin/function/applicant_delete.php
@@ -3,20 +3,41 @@
 	require_once '../includes/session.php';
 
 	if(isset($_POST['id'])){
+
 		//get pass
 		$pass = $_POST['pass'];
 		$employee_id = $user['employee_id'];
+    //get id
+    $id = $_POST['id'];
 
 		//challenge
 		if (password_verify($pass,get_password($employee_id,$conn))) {
+
       //delete (prepared stmt)
 			$sql = $conn->prepare("DELETE FROM applicant WHERE id = ? ");
       $sql->bind_param('s',$id);
-      //get id
-      $id = $_POST['id'];
-			echo ($sql->execute())? 1:0;
+
+      // delete resume file
+      $resume = $conn->prepare("SELECT attachment FROM applicant WHERE id= ? ");
+      $resume->bind_param('s',$id);
+      $resume->execute();
+      $result = $resume->get_result();
+      $row = $result->fetch_assoc();
+      $filename = $row['attachment'];
+
+      //DELETE EXISTING FILE
+      if(file_exists("../uploads/applicant/".$filename)){
+        if (unlink("../uploads/applicant/".$filename)) {
+            echo ($sql->execute())? 1:0;
+        } else {
+          echo 0; //error
+        }
+      } else {
+        echo 0; //error
+      }
+
 		}else{
-			echo 2;
+			echo 2; // wrong password
 		}
 		
 	}else{
