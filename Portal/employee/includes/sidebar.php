6c3532f386ce868b94be3b07863889f67696c9f9
Francis Ong <francis.ong25@gmail.com>
2022-04-18 15:04:18 +0800

    fix indention

--- Portal/employee/includes/sidebar.php
+++ Portal/employee/includes/sidebar.php
@@ -6,175 +6,204 @@
   window.location.replace('../../index.php');
 <?php }} ?>
 </script>
-          <div class="pcoded-main-container">
-              <div class="pcoded-wrapper">
-                  <nav class="pcoded-navbar">
-                      <div class="sidebar_toggle"><a href="#"><i class="icon-close icons"></i></a></div>
-                      <div class="pcoded-inner-navbar main-menu">
-                          <div class="">
-                              <div class="main-menu-header">
-                                  <img class="img-80 img-radius" src="/Portal/admin/images/<?php echo (!empty($user['photo']))?$user['photo']:'profile.jpg'; ?>" width="150" height="60" alt="User-Profile-Image">
-                                  <div class="user-details">
-                                      <span id="more-details"><?php echo $user['firstname']; ?><i class="fa fa-caret-down"></i></span>
-                                  </div>
-                              </div>
-        
-                              <div class="main-menu-content">
-                                  <ul>
-                                      <li class="more-details">
-                                          <a href="profile.php" onclick="store('#personal')"><i class="ti-user"></i>View Profile</a>
-                                          <a href="profile.php" onclick="store('#setting')"><i class="ti-settings"></i>Settings</a>
-                                          <a href="logout.php"><i class="ti-layout-sidebar-left"></i>Logout</a>
-                                      </li>
-                                  </ul>
-                              </div>
-                          </div>
-                          <!-- <div class="p-15 p-b-0">
-                              <form class="form-material">
-                                  <div class="form-group form-primary">
-                                      <input type="text" name="footer-email" class="form-control" required="">
-                                      <span class="form-bar"></span>
-                                      <label class="float-label"><i class="fa fa-search m-r-10"></i>Search</label>
-                                  </div>
-                              </form>
-                          </div>-->
-                          <div class="pcoded-navigation-label" data-i18n="nav.category.navigation">Reports</div>
-                          <ul class="pcoded-item pcoded-left-item nav">
-                              <li class="nav-item">
-                                  <a href="index.php" class="waves-effect waves-dark">
-                                      <span class="pcoded-micon"><i class="ti-home"></i><b>D</b></span>
-                                      <span class="pcoded-mtext" data-i18n="nav.dash.main">Dashboard</span>
-                                      <span class="pcoded-mcaret"></span></a>
-                              </li>
-                          
-                              <div class="pcoded-navigation-label" data-i18n="nav.category.forms">Manage</div>
-                              
-                              <li>
-                                <a href="schedule.php" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="ti-calendar"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext" data-i18n="nav.form-components.main">Schedule</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
-                              
-                              <li>
-                                <a href="dtr.php" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="ti-timer"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext" data-i18n="nav.form-components.main">Attendance</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
 
+<div class="pcoded-main-container">
+  <div class="pcoded-wrapper">
+    <nav class="pcoded-navbar">
+      <div class="sidebar_toggle"><a href="#"><i class="icon-close icons"></i></a></div>
+      <div class="pcoded-inner-navbar main-menu">
+        <div class="">
+          <div class="main-menu-header">
+            <img class="img-80 img-radius" src="../admin/uplaods/profile/<?php echo (!empty($user['photo']))?$user['photo']:'profile.jpg'; ?>" width="150" height="60" alt="User-Profile-Image">
+            <div class="user-details">
+              <span id="more-details"><?php echo $user['firstname']; ?>
+                <i class="fa fa-caret-down"></i>
+              </span>
+            </div>
+          </div>
+          <div class="main-menu-content">
+            <ul>
+              <li class="more-details">
+                <a href="profile.php" onclick="store('#personal')">
+                  <i class="ti-user"></i>View Profile
+                </a>
+                <a href="profile.php" onclick="store('#setting')">
+                  <i class="ti-settings"></i>Settings
+                </a>
+                <a href="logout.php">
+                  <i class="ti-layout-sidebar-left"></i>Logout
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+                       
+        <div class="pcoded-navigation-label" data-i18n="nav.category.navigation">
+          Reports
+        </div>
 
-                            <li>
-                                <a href="overtime.php" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="fa fa-clock-o"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext" data-i18n="nav.form-components.main">Overtime</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
+        <ul class="pcoded-item pcoded-left-item nav">
+          <li class="nav-item">
+            <a href="index.php" class="waves-effect waves-dark">
+              <span class="pcoded-micon"><i class="ti-home"></i><b>D</b></span>
+              <span class="pcoded-mtext" data-i18n="nav.dash.main">Dashboard</span>
+              <span class="pcoded-mcaret"></span>
+            </a>
+          </li>
+          <div class="pcoded-navigation-label" data-i18n="nav.category.forms">
+            Manage
+          </div>
+          <li>
+            <a href="schedule.php" class="waves-effect waves-dark">
+              <span class="pcoded-micon">
+                <i class="ti-calendar"></i><b>FC</b>
+              </span>
+              <span class="pcoded-mtext" data-i18n="nav.form-components.main">      
+                Schedule
+              </span>
+              <span class="pcoded-mcaret"></span>
+            </a>
+          </li>
+          <li>
+            <a href="dtr.php" class="waves-effect waves-dark">
+              <span class="pcoded-micon"><i class="ti-timer"></i><b>FC</b></span>
+              <span class="pcoded-mtext" data-i18n="nav.form-components.main">
+                Attendance
+              </span>
+              <span class="pcoded-mcaret"></span>
+            </a>
+          </li>
+          <li>
+            <a href="overtime.php" class="waves-effect waves-dark">
+              <span class="pcoded-micon">
+                <i class="fa fa-clock-o"></i><b>FC</b>
+              </span>
+              <span class="pcoded-mtext" data-i18n="nav.form-components.main">
+                Overtime
+              </span>
+              <span class="pcoded-mcaret"></span>
+            </a>
+          </li>
+          <li>
+            <a href="official_business.php" class="waves-effect waves-dark">
+              <span class="pcoded-micon">
+                <i class="fa fa-id-card-o"></i><b>FC</b>
+              </span>
+              <span class="pcoded-mtext" data-i18n="nav.form-components.main">
+                Official Business
+              </span>
+              <span class="pcoded-mcaret"></span>
+            </a>
+          </li>
+          <li>
+            <a href="cash_advance.php" class="waves-effect waves-dark">
+              <span class="pcoded-micon">
+                <i class="fa fa-money"></i><b>FC</b>
+              </span>
+              <span class="pcoded-mtext" data-i18n="nav.form-components.main">
+                Cash Advance
+              </span>
+              <span class="pcoded-mcaret"></span>
+            </a>
+          </li>
+          <li>
+            <a href="payroll_summary.php" class="waves-effect waves-dark">
+              <span class="pcoded-micon">
+                <i class="ti-money"></i><b>FC</b>
+              </span>
+              <span class="pcoded-mtext" data-i18n="nav.form-components.main">
+                Payslip
+              </span>
+              <span class="pcoded-mcaret"></span>
+            </a>
+          </li>
+          <li>
+            <a href="disciplinary.php" class="waves-effect waves-dark">
+              <span class="pcoded-micon"><i class="ti-receipt"></i><b>FC</b></span>
+              <span class="pcoded-mtext" data-i18n="nav.form-components.main">  
+                Diciplinary
+              </span>
+              <span class="pcoded-mcaret"></span>
+            </a>
+          </li>
+          <li>
+            <a href="benefits.php" class="waves-effect waves-dark">
+              <span class="pcoded-micon"><i class="ti-gift"></i><b>FC</b></span>
+              <span class="pcoded-mtext" data-i18n="nav.form-components.main">
+                Benefits
+              </span>
+              <span class="pcoded-mcaret"></span>
+            </a>
+          </li>
+           <li>
+            <a href="tasks.php" class="waves-effect waves-dark">
+              <span class="pcoded-micon">
+                <i class="ti-flag-alt-2"></i><b>FC</b>
+              </span>
+              <span class="pcoded-mtext" data-i18n="nav.form-components.main">
+                Tasks
+              </span>
+              <span class="pcoded-mcaret"></span>
+            </a>
+          </li>
+          <li>
+            <a href="training.php" class="waves-effect waves-dark">
+              <span class="pcoded-micon"><i class="ti-stats-up"></i><b>FC</b></span>
+              <span class="pcoded-mtext" data-i18n="nav.form-components.main">
+                Trainings
+              </span>
+              <span class="pcoded-mcaret"></span>
+            </a>
+          </li>
+          <li>
+            <a href="documents.php" class="waves-effect waves-dark">
+              <span class="pcoded-micon"><i class="ti-support"></i><b>FC</b></span>
+              <span class="pcoded-mtext" data-i18n="nav.form-components.main">
+                Document Request
+              </span>
+              <span class="pcoded-mcaret"></span>
+            </a>
+          </li>
+          <li>
+            <a href="events.php" class="waves-effect waves-dark">
+              <span class="pcoded-micon"><i class="ti-agenda"></i><b>FC</b></span>
+              <span class="pcoded-mtext" data-i18n="nav.form-components.main">
+                Event Request
+              </span>
+              <span class="pcoded-mcaret"></span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
 
-                            <li>
-                                <a href="official_business.php" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="fa fa-id-card-o"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext" data-i18n="nav.form-components.main">Official Business</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
-                            
-                        
- 
-                            <li>
-                                <a href="cash_advance.php" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="fa fa-money"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext" data-i18n="nav.form-components.main">Cash Advance</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="payroll_summary.php" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="ti-money"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext" data-i18n="nav.form-components.main">Payslip</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="disciplinary.php" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="ti-receipt"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext" data-i18n="nav.form-components.main">Diciplinary</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="benefits.php" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="ti-gift"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext" data-i18n="nav.form-components.main">Benefits</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
-                             <li>
-                                <a href="tasks.php" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="ti-flag-alt-2"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext" data-i18n="nav.form-components.main">Tasks</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="training.php" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="ti-stats-up"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext" data-i18n="nav.form-components.main">Trainings</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="documents.php" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="ti-support"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext" data-i18n="nav.form-components.main">Document Request</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="events.php" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="ti-agenda"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext" data-i18n="nav.form-components.main">Event Request</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
-        
-                          </ul>
-        
-                          
-                      </div>
-                  </nav>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-      <script>
-        // STORE PAGE LINK TO SESSION STORAGE
-        function store(id){
-           sessionStorage.setItem('activeTab', id);
-        }
+<script>
 
-        $(document).ready(function (){
-        
-          // CHECK THE CURRENT PAGE FOR ACTIVE LINK
-          var url = window.location; // get location for basis
-          var element = $('ul.nav a').filter(function() {
-            return this.href == url || url.href.indexOf(this.href) == 0; // check if the url is "IN" href
-          }).addClass('active').parent().addClass('in');
-          if (element.is('li')) {
-            $('.nav li.active').removeClass('active'); // REMOVE EXISTING
-            element.addClass('active'); // ACTIVATE ACTIVE
-            //for nested li (e.g Employee)
-            var emp = (url).href.indexOf("employee") != -1 || (url).href.indexOf("overtime") != -1 || (url).href.indexOf("cash") != -1 || (url).href.indexOf("deduction") != -1 || (url).href.indexOf("position") != -1 || (url).href.indexOf("schedule") != -1 || (url).href.indexOf("benefits") != -1 || (url).href.indexOf("disciplinary") != -1;
-            if (emp){
-              $("#employee").addClass('active'); // ACTIVATE ACTIVE
-            }else if ((url).href.indexOf("evaluation") != -1){
-              $("#evaluation").addClass('active'); // ACTIVATE ACTIVE
-            }
-          }
+  // STORE PAGE LINK TO SESSION STORAGE
+  function store(id){
+    sessionStorage.setItem('activeTab', id);
+  }
 
+  $(document).ready(function (){
+    // CHECK THE CURRENT PAGE FOR ACTIVE LINK
+    var url = window.location; // get location for basis
+    var element = $('ul.nav a').filter(function() {
+      return this.href == url || url.href.indexOf(this.href) == 0; // check if the url is "IN" href
+    }).addClass('active').parent().addClass('in');
+    if (element.is('li')) {
+      $('.nav li.active').removeClass('active'); // REMOVE EXISTING
+      element.addClass('active'); // ACTIVATE ACTIVE
+      //for nested li (e.g Employee)
+      var emp = (url).href.indexOf("employee") != -1 || (url).href.indexOf("overtime") != -1 || (url).href.indexOf("cash") != -1 || (url).href.indexOf("deduction") != -1 || (url).href.indexOf("position") != -1 || (url).href.indexOf("schedule") != -1 || (url).href.indexOf("benefits") != -1 || (url).href.indexOf("disciplinary") != -1;
+      if (emp){
+        $("#employee").addClass('active'); // ACTIVATE ACTIVE
+      }else if ((url).href.indexOf("evaluation") != -1){
+        $("#evaluation").addClass('active'); // ACTIVATE ACTIVE
+      }
+    }
 
+  });
 
-        });
-
-      </script>
+</script>
