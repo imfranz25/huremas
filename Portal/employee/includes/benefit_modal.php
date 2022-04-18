1dd1cbeb1f81493168d1e9d2d81ac80b61365a03
Francis Ong <francis.ong25@gmail.com>
2022-04-18 13:44:08 +0800

    fix indention

--- Portal/employee/includes/benefit_modal.php
+++ Portal/employee/includes/benefit_modal.php
@@ -1,37 +1,31 @@
-
 <!-- View -->
 <div class="modal fade" id="view_desc">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-              
-              <h4 class="modal-title"><b>View Benefit</b></h4>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-            </div>
-
-            <div class="modal-body">
-
-                <div class="form-group row">
-                  <label class="col-2 float-label">Title</label>
-                  <div class="col-10">
-                    <input type="text"  class="form-control border border-secondary edit_title" readonly>
-                  </div>
-                </div>
-
-
-                <div class="form-group row">
-                  <label class="col-2 float-label">Description</label>
-                  <div class="col-10">
-                    <textarea class="form-control border border-secondary edit_description" readonly rows="8" ></textarea>
-                  </div>
-                </div>
-
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
-            </div>
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title"><b>View Benefit</b></h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-group row">
+          <label class="col-2 float-label">Title</label>
+          <div class="col-10">
+            <input type="text"  class="form-control border border-secondary edit_title" readonly>
+          </div>
         </div>
+        <div class="form-group row">
+          <label class="col-2 float-label">Description</label>
+          <div class="col-10">
+            <textarea class="form-control border border-secondary edit_description" readonly rows="8" ></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+      </div>
     </div>
+  </div>
 </div>
 
