dc65942a0a900119f642d7cf7dd2773ace0ed263
Francis Ong <francis.ong25@gmail.com>
2022-04-18 14:40:47 +0800

    fix indention

--- Portal/employee/includes/script.php
+++ Portal/employee/includes/script.php
@@ -1,30 +1,26 @@
 <script>
-window.uni_modal = function($title = '' , $url='',$size=""){
-	      start_load()
-	      $.ajax({
-	          url:$url,
-	          error:err=>{
-	              console.log()
-	              alert("An error occured")
-	          },
-	          success:function(resp){
-	              if(resp){
-	                  $('#uni_modal .modal-title').html($title)
-	                  $('#uni_modal .modal-body').html(resp)
-	                  if($size != ''){
-	                      $('#uni_modal .modal-dialog').addClass($size)
-	                  }else{
-	                      $('#uni_modal .modal-dialog').removeAttr("class").addClass("modal-dialog modal-md")
-	                  }
-	                  $('#uni_modal').modal({
-	                    show:true,
-	                    backdrop:'static',
-	                    keyboard:false,
-	                    focus:true
-	                  })
-	                  end_load()
-	              }
-	          }
-	      })
-	  }
+  window.uni_modal = function($title = '' , $url='',$size=""){
+    start_load();
+    $.ajax({
+      url:$url,
+      success:function(resp){
+        if(resp){
+          $('#uni_modal .modal-title').html($title)
+          $('#uni_modal .modal-body').html(resp)
+          if($size != ''){
+            $('#uni_modal .modal-dialog').addClass($size)
+          }else{
+            $('#uni_modal .modal-dialog').removeAttr("class").addClass("modal-dialog modal-md")
+          }
+          $('#uni_modal').modal({
+            show:true,
+            backdrop:'static',
+            keyboard:false,
+            focus:true
+          })
+          end_load();
+        }
+      }
+    })
+  }
 </script>
